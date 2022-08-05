c312fac9569ddd90b89b5ab2e553b62a29b9522a
José Antonio Carrera E <aries544@hotmail.com>
2022-08-05 15:46:40 -0500

    Se crea la conexión a la BDD y se crean los Modelos a utilizar

---
+++ Backend/USFQ_Backend/USFQ_Backend/Models/Contacto.cs
@@ -0,0 +1,10 @@
+﻿namespace USFQ_Backend.Models
+{
+    public class Contacto
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Telefono { get; set; }
+        public string? Correo { get; set; }
+    }
+}
---
+++ Backend/USFQ_Backend/USFQ_Backend/Models/Transaccion.cs
@@ -0,0 +1,12 @@
+﻿namespace USFQ_Backend.Models
+{
+    public class Transaccion
+    {
+        public int Id { get; set; }
+        public int ContactoId { get; set; }
+        public DateTime FechaTransaccion { get; set; }
+        public decimal Monto { get; set; }
+        public int TipoOperacion { get; set; }
+        public string Nota { get; set; }
+    }
+}
